e19a2401d38e8642484b2197277954780a8cb1ab
Amit Moryossef <amitmoryossef@gmail.com>
2025-01-03 16:03:21 +0100

    feat(gemini): use new prompt template

--- sign_gpt/models/data.py
+++ sign_gpt/models/data.py
@@ -4,11 +4,11 @@ from pathlib import Path
 from datasets import load_dataset
 
 
-def load_split_data(split, mapping_fn):
+def load_split_data(split, mapping_fn, **kwargs):
     processed_dir = Path(__file__).parent.parent.parent / 'processed'
     data_files = str(processed_dir / '*' / f'*.{split}.jsonl.gz')
     dataset = load_dataset('json', data_files=data_files, split='train')
-    return dataset.map(mapping_fn, num_proc=os.cpu_count())
+    return dataset.map(mapping_fn, num_proc=os.cpu_count(), **kwargs)
 
 
 def dataset_test_mapping_fn(datum, mapping_fn):
@@ -17,13 +17,13 @@ def dataset_test_mapping_fn(datum, mapping_fn):
     return mapping_fn(datum)
 
 
-def load_datasets(mapping_fn, hide_test=True):
+def load_datasets(mapping_fn, hide_test=True, **kwargs):
     # Load train, dev, and test splits
-    train_dataset = load_split_data('train', mapping_fn)
-    validation_dataset = load_split_data('validation', mapping_fn)
+    train_dataset = load_split_data('train', mapping_fn, **kwargs)
+    validation_dataset = load_split_data('validation', mapping_fn, **kwargs)
 
     test_mapping = (lambda datum: dataset_test_mapping_fn(datum, mapping_fn)) if hide_test else mapping_fn
-    test_dataset = load_split_data('test', test_mapping)
+    test_dataset = load_split_data('test', test_mapping, **kwargs)
 
     return train_dataset, validation_dataset, test_dataset
 
--- sign_gpt/models/vertex/gemini.py
+++ sign_gpt/models/vertex/gemini.py
@@ -3,16 +3,22 @@ from vertexai.preview.tuning import sft
 
 
 def prep_gemini_instruction(datum):
-    messages = [{"role": "system", "content": datum['system']}]
+    messages = []
     for message in datum['messages']:
-        messages.append({"role": "user", "content": message['input']})
-        messages.append({"role": "model", "content": message['output']})
+        messages.append({"role": "user", "parts": [{"text": message['input']}]})
+        messages.append({"role": "model", "parts": [{"text": message['output']}]})
 
-    if any(message["content"] == "" for message in messages):
-        print(messages)
+    if any(message["parts"][0]["text"] == "" for message in messages):
+        print(message)
         raise Exception("Empty input or output will make gemini crash")
 
-    return {"messages": messages, "system": None}  # Override system with None
+    return {
+        "systemInstruction": {
+            "role": "system",
+            "parts": [{"text": datum['system']}]
+        },
+        "contents": messages
+    }
 
 
 if __name__ == "__main__":
@@ -20,12 +26,14 @@ if __name__ == "__main__":
     gs_path = "gs://sign-language-datasets/public/sign-gpt"
 
     # Upload datasets to GCS
-    # train_dataset, validation_dataset, test_dataset = load_datasets(prep_gemini_instruction, hide_test=False)
-    # train_dataset.to_json(gcs_path + "/train.jsonl", lines=True)
-    # # Validation Dataset can not contain more than 256 model turns.
-    # validation_dataset = validation_dataset.select(range(256))
-    # validation_dataset.to_json(gcs_path + "/validation.jsonl", lines=True)
-    # test_dataset.to_json(gcs_path + "/test.jsonl", lines=True)
+    train_dataset, validation_dataset, test_dataset = load_datasets(prep_gemini_instruction, hide_test=False,
+                                                                    remove_columns=("system", "messages"))
+
+    train_dataset.to_json(gcs_path + "/train.jsonl", lines=True)
+    # Validation Dataset can not contain more than 256 model turns.
+    validation_dataset = validation_dataset.select(range(256))
+    validation_dataset.to_json(gcs_path + "/validation.jsonl", lines=True)
+    test_dataset.to_json(gcs_path + "/test.jsonl", lines=True)
 
     # TODO: max allowed training steps is 50,000, with an unknown batch size.
     #       this makes vertex ai tuning on our entire dataset impossible.
@@ -33,7 +41,7 @@ if __name__ == "__main__":
     # Create training job
     vertexai.init(project="sign-mt", location="us-central1")
     sft_tuning_job = sft.train(
-        source_model="gemini-1.0-pro-002",
+        source_model="gemini-1.5-pro-002",
         train_dataset=gs_path + "/train.jsonl",
         validation_dataset=gs_path + "/validation.jsonl",
         epochs=1,
